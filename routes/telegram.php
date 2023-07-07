dacfca1b5cba4194edb8ad712509f274583d8c5e
Mohir <mohirkarimov4181@gmail.com>
2023-07-07 11:48:52 +0500

    example_new

--- routes/telegram.php
+++ routes/telegram.php
@@ -77,7 +77,7 @@ $bot->onCommand('start', function(Nutgram $bot){
     $bot->sendMessage('Welcome!', [
         'reply_markup' => ForceReply::make(
             force_reply: true,
-            input_field_placeholder: 'dfdfdfdfdfd',
+            input_field_placeholder: 'woiwwroowrkwokwiwoeiwoei',
             selective: true,
         ),
     ]);
