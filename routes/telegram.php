b66c1a4881f27005e7a6c98c6a48ee4e4c054c2d
Mohir <mohirkarimov4181@gmail.com>
2023-07-07 11:19:50 +0500

    keyboards

--- routes/telegram.php
+++ routes/telegram.php
@@ -13,6 +13,72 @@ use SergiX44\Nutgram\Nutgram;
 |
 */
 
-$bot->onCommand('start', function (Nutgram $bot) {
-    return $bot->sendMessage('Hello, world!');
-})->description('The start command!');
+//$bot->onCommand('start', function (Nutgram $bot) {
+//    return $bot->sendMessage('Hello, world!');
+//})->description('The start command!');
+
+
+
+
+////    Inline Keyboard   ///////
+
+$bot->onCommand('start', function(Nutgram $bot){
+    $bot->sendMessage('Welcome!', [
+        'reply_markup' => InlineKeyboardMarkup::make()
+            ->addRow(
+                InlineKeyboardButton::make('A', callback_data: 'type:a'),
+                InlineKeyboardButton::make('B', callback_data: 'type:b')
+            )
+    ]);
+});
+
+$bot->onCallbackQueryData('type:a', function(Nutgram $bot){
+    $bot->answerCallbackQuery([
+        'text' => 'You selected A'
+    ]);
+});
+
+$bot->onCallbackQueryData('type:b', function(Nutgram $bot){
+    $bot->answerCallbackQuery([
+        'text' => 'You selected B'
+    ]);
+});
+
+//////////////////////////   Reply Keyboard    //////////////////////////////////////
+
+$bot->onCommand('start', function(Nutgram $bot){
+    $bot->sendMessage('Welcome!', [
+        'reply_markup' => ReplyKeyboardMarkup::make()->addRow(
+            KeyboardButton::make('Give me food!'),
+            KeyboardButton::make('Give me animal!'),
+        )
+    ]);
+});
+
+$bot->onText('Give me food!', function (Nutgram $bot) {
+    $bot->sendMessage('Apple!');
+});
+
+$bot->onText('Give me animal!', function (Nutgram $bot) {
+    $bot->sendMessage('Dog!');
+});
+
+/////////////////////////////////  Remove the Reply Keyboard   //////////////////////////////////////
+
+$bot->onCommand('cancel', function (Nutgram $bot) {
+    $bot->sendMessage('Removing keyboard...', [
+        'reply_markup' => ReplyKeyboardRemove::make(true),
+    ])?->delete();
+});
+
+////////////////////////////////         Force Reply             ///////////////////////////////////////
+
+$bot->onCommand('start', function(Nutgram $bot){
+    $bot->sendMessage('Welcome!', [
+        'reply_markup' => ForceReply::make(
+            force_reply: true,
+            input_field_placeholder: 'Type something',
+            selective: true,
+        ),
+    ]);
+});
