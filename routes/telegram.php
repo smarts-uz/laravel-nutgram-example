9e9371921b2bbd567feaf8dfa9ad5dbac7bf1806
Mohir <mohirkarimov4181@gmail.com>
2023-07-08 11:20:48 +0500

    keyboards

--- routes/telegram.php
+++ routes/telegram.php
@@ -35,8 +35,7 @@ use App\Telegram\SendingRequest\SendMessage;
 use App\Telegram\SendingRequest\SendPhoto;
 use App\Telegram\SendingRequest\SendDocument;
 use App\Telegram\SendingRequest\SendSticker;
-use App\Telegram\SendingRequest\SendHelloMessage;
-use App\Telegram\SendingRequest\SendVoice;
+
 
 
 use SergiX44\Nutgram\Telegram\Attributes\MessageTypes;
@@ -91,9 +90,6 @@ $bot->onCommand('video', SendVideo::class)->description('Sending video');
 $bot->onCommand('photo', SendPhoto::class)->description('Sending photo');
 $bot->onCommand('doc', SendDocument::class)->description('Sending document');
 $bot->onCommand('sticker', SendSticker::class)->description('Sending sticker');
-
-$bot->onCommand('voice', SendVoice::class)->description('Sending voice');
-$bot->onCommand('hello', SendHelloMessage::class)->description('Sending hello message');
 /*
 |--------------------------------------------------------------------------
 | Exception handlers
